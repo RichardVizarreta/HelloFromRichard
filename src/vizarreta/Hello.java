4ef3a215abb906a7c5af76427a113d8403716603
Richard Vizarreta <vizarretari27@byui.edu>
2021-10-14 14:29:59 -0600

    Exclamation

--- src/vizarreta/Hello.java
+++ src/vizarreta/Hello.java
@@ -2,6 +2,6 @@ package vizarreta;
 
 public class Hello {
     public static void main (String[] arg) {
-        System.out.println("Hello from Richard");
+        System.out.println("Hello from Richard!");
     }
 }
