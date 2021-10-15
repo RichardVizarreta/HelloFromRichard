c299b19fe0d32fd2d36093a06e99c00f43429ab5
Blaine <van17021@byui.edu>
2021-10-15 11:01:29 -0600

    Changed to reply to initial hello message.

--- src/vizarreta/Hello.java
+++ src/vizarreta/Hello.java
@@ -3,5 +3,6 @@ package vizarreta;
 public class Hello {
     public static void main (String[] arg) {
         System.out.println("Hello from Richard!");
+        System.out.println("Hello Richard, my name is Blaine");
     }
 }
